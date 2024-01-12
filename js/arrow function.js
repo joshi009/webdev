4c6ad6d6e8deb364fb548f078946bf64025a7cc8
kalyan joshi <joshi1307kashyap@gmail.com>
2024-01-12 22:28:39 +0530

    webdev

---
+++ js/arrow function.js
@@ -0,0 +1,20 @@
+    //developing Arrow function 
+
+    const  myfun=()=>{
+        document.write("hi,i am arrow function<br>");
+
+    }
+
+
+    // AF with params &{}
+    const add =(X,Y)=>{
+        return X+Y;
+
+    }
+
+
+    //AF with params & without {}
+      const power =(X,Y) => X**Y;   //impact retun
+
+      //AF with param & W/o {}
+      const natsum = n => n*(n+1)/2;
---
+++ js/expression function.js
@@ -0,0 +1,13 @@
+    //developing expressional function 
+
+    const add =function(a,b)
+    {
+        return (a+b);
+
+    }
+
+
+    const product = function(x,y)
+    {
+        return x*y;
+    }
